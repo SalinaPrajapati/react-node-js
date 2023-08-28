a76755234553da27ab093b3af17634398a357e97
SalinaPrajapati <saleenaprajapati1@gmail.com>
2023-08-28 22:06:15 +0545

    react project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { PrimeReactProvider } from './primereact/context';
 
-function App() {
+function App( Component, pageProps ) {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +19,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+         <PrimeReactProvider>
+        <Component {...pageProps} />
+    </PrimeReactProvider>
+      </div>
     </div>
   );
-}
+}        
 
 export default App;
